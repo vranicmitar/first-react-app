f40db8cdd2310ecd5d34c2c9d70f0869bd3d2147
vranicmitar <mitarvranic0@gmail.com>
2023-04-08 13:21:52 +0200

    #update Cards

--- my-first-app/src/components/Cards/PersonCard.js
+++ my-first-app/src/components/Cards/PersonCard.js
@@ -8,10 +8,34 @@ function PersonCard() {
       <div className="cards">
         <div className="prvi">
           <img src="https://www.slikomania.rs/fotky6509/fotos/slike-na-platnu_XOBZV030E11E11-gal2.jpg"></img>
-          <h2>Mitar Vranic</h2>
+          <h2 id="naslov">Mitar Vranic</h2>
           <h3>Novi Pazar</h3>
           <p>IT camp</p>
-          <FaGithub style={"width: 150px, height:150px"} />
+          <FaGithub className="icon" />
+        </div>
+
+        <div className="drugi">
+          <img src="https://www.slikomania.rs/fotky6509/fotos/slike-na-platnu_XOBZV030E11E11-gal2.jpg"></img>
+          <h2 id="naslov">Mitar Vranic</h2>
+          <h3>Novi Pazar</h3>
+          <p>IT camp</p>
+          <FaGithub className="icon" />
+        </div>
+
+        <div className="treci">
+          <img src="https://www.slikomania.rs/fotky6509/fotos/slike-na-platnu_XOBZV030E11E11-gal2.jpg"></img>
+          <h2 id="naslov">Mitar Vranic</h2>
+          <h3>Novi Pazar</h3>
+          <p>IT camp</p>
+          <FaGithub className="icon" />
+        </div>
+
+        <div className="cetvrti">
+          <img src="https://www.slikomania.rs/fotky6509/fotos/slike-na-platnu_XOBZV030E11E11-gal2.jpg"></img>
+          <h2 id="naslov">Mitar Vranic</h2>
+          <h3>Novi Pazar</h3>
+          <p>IT camp</p>
+          <FaGithub className="icon" />
         </div>
       </div>
     </>
